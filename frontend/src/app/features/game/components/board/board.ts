b3ed01c62ea6de980c833b10e3720a9931821718
dstet <me@dstet.me>
2026-02-18 22:01:17 -0500

    Redo board and piece CSS/layout to support draggability. Add mock endpoint for updating board state.

--- frontend/src/app/features/game/components/board/board.ts
+++ frontend/src/app/features/game/components/board/board.ts
@@ -1,28 +1,33 @@
 import { Component, effect, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 import { BoardLoadService } from '../../services/board-load-service';
-import { Observable } from 'rxjs';
 import { Piece } from '../piece/piece';
 import { ChessPiece } from '../../services/pieces/ChessPiece';
-import { CdkDrag } from '@angular/cdk/drag-drop';
+import { Position } from '../../services/pieces/Position';
 
 @Component({
   selector: 'app-board',
-  imports: [AsyncPipe, Piece, CdkDrag],
+  imports: [AsyncPipe, Piece],
   templateUrl: './board.html',
   styleUrl: './board.css',
 })
 export class Board {
-  rows = Array.from({ length: 8 });
-  cols = Array.from({ length: 8 });
+  grid = Array.from({ length: 64 });
 
   private loadService = inject(BoardLoadService);
-  boardState$: Observable<Map<string, ChessPiece>> = this.loadService.boardLoad();
+  boardState$: Observable<ChessPiece[]> = this.loadService.boardLoad();
 
   constructor() {}
 
-  getItem(itemMap: Map<string, ChessPiece>, row: number, col: number): ChessPiece | null {
-    return itemMap.get(`${row},${col}`) ?? null;
+  onPieceMoved(piece: ChessPiece, target: Position) {
+    this.boardState$ = this.loadService.updatePiecePosition(piece, target)
+  }
+
+  isDarkSquare(i: number): boolean {
+    const row = Math.floor(i / 8)
+    const col = i % 8;
+    return (row + col) % 2 !== 0
   }
 }
--- frontend/src/app/features/game/services/board-load-service.ts
+++ frontend/src/app/features/game/services/board-load-service.ts
@@ -1,10 +1,16 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
+
 import { Bishop, ChessPiece, King, Knight, Pawn, Queen, Rook } from './pieces/ChessPiece';
+import { Position } from './pieces/Position';
 
 interface BoardApiRequest {
   fenString: string
+}
+
+interface UpdatedRequest {
+  arbitraryValue: boolean
 }
 
 @Injectable({
@@ -12,40 +18,59 @@ interface BoardApiRequest {
 })
 export class BoardLoadService {
   private http = inject(HttpClient);
+  
+  positionsArray: ChessPiece[]|null = null;
 
   /**
    * @returns {Map<string, ChessPiece} Indexed map of pieces on board with key of "[Position.x],[Position.y]".
    */
-  boardLoad(): Observable<Map<string, ChessPiece>> {
+  boardLoad(): Observable<ChessPiece[]> {
     // Returns an observable after sequentially decoding JSON string and filtering into the map via rxjs pipe.
     return this.http.get<BoardApiRequest>(`http://127.0.0.1:5000/api/mock-board`).pipe(
-      map(state => this.fenDecode(state.fenString)), 
-      map(items => {
-          const itemMap = new Map<string, ChessPiece>();
-          items.forEach(item => itemMap.set(`${item.position.x},${item.position.y}`, item))
-          return itemMap;
-        }
-      )
+      map(state => this.fenDecode(state.fenString))
     );
+  }
+
+  updatePiecePosition(piece: ChessPiece, newPos: Position): Observable<ChessPiece[]> {
+    return this.http.get<UpdatedRequest>(`http://127.0.0.1:5000/api/update-board`).pipe(
+      map(_state => this.updatePos(piece, newPos))
+    )
+  }
+
+  private updatePos(piece: ChessPiece, newPos: Position): ChessPiece[] {
+    console.log(`${piece.id} new pos: ${newPos.x}, ${newPos.y}`)
+    if (this.positionsArray !== null) {
+      this.positionsArray[piece.id].position = newPos;
+    } else {
+      console.warn("Attempted to update pos before init")
+      this.positionsArray = this.mockPositions();
+    }
+
+    return this.positionsArray;
+  }
+
+  private mockPositions(): ChessPiece[] {
+    return [
+      new Rook(0, false, 0, 0),
+      new Knight(1, false, 0, 1),
+      new Bishop(2, false, 0, 2),
+      new Queen(3, false, 0, 3),
+      new King(4, false, 0, 4),
+      new Pawn(5, false, 1, 0),
+      new Pawn(6, true, 6, 0),
+      new Rook(7, true, 7, 0),
+      new Knight(8, true, 7, 1),
+      new Bishop(9, true, 7, 2),
+      new Queen(10, true, 7, 3),
+      new King(11, true, 7, 4),
+    ]
   }
 
   private fenDecode(fenString: string): ChessPiece[] {
     // TODO add proper decoding. https://github.com/dstettler/simpleboard/issues/35
     // Mock return.
-    console.log(fenString);
-    return [
-      new Rook(false, 0, 0),
-      new Knight(false, 0, 1),
-      new Bishop(false, 0, 2),
-      new Queen(false, 0, 3),
-      new King(false, 0, 4),
-      new Pawn(false, 1, 0),
-      new Pawn(true, 6, 0),
-      new Rook(true, 7, 0),
-      new Knight(true, 7, 1),
-      new Bishop(true, 7, 2),
-      new Queen(true, 7, 3),
-      new King(true, 7, 4),
-    ]
+    console.log(`fenString: ${fenString}`);
+    this.positionsArray = this.mockPositions();
+    return this.positionsArray
   }
 }
