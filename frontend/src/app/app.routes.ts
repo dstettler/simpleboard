f7d2c88984fc23caac87f02ca14f63dfcf0dee07
sreeramgvp11 <sreeramgangavarapu2004@gmail.com>
2026-03-12 23:21:59 -0400

    Updated the home page with tailwind css

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login';
-;
+import { HomeComponent } from './features/home/home';
 import { DashboardComponent } from './dashboard/dashboard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: '**', redirectTo: 'login' }
+  { path: '**', redirectTo: '' }
 ];
