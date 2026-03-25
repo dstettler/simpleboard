c89f03d12fac4473bc18cc9d347a3dba3f66534e
dstet <me@dstet.me>
2026-03-25 17:18:08 -0400

    Add game path

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login';
 import { HomeComponent } from './features/home/home';
 import { DashboardComponent } from './dashboard/dashboard';
+import { Game } from './features/game/game';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'game', component: Game },
   { path: '**', redirectTo: '' }
 ];
